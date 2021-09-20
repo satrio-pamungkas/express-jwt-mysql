5b1e9fb610018fd0b319b77c355dcd9077699998
aderio25 <adewipo@gmail.com>
2021-09-20 10:21:56 +0700

    initial commit

---
+++ app/models/user.model.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+    });
+
+    return User;
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express()
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+    console.log('Drop and Resync db');
+    initial();
+});
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+    res.json({ message: "It worked" });
+});
+
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
